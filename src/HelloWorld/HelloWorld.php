345e7faeb9884fc2e6f0302fc5c55e7e568ef819
Pete Lawrence <pete@carltonsoftware.co.uk>
2014-03-03 14:16:40 +0000

    Implemented PSR-4 autoloading

---
+++ src/HelloWorld/HelloWorld.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * A test class
+ *
+ * PHP version 5
+ *
+ * @category HelloWorld
+ * @package  HelloWorld
+ * @author   Pete Lawrence <pete@carltonsoftware.co.uk>
+ * @license  All rights reserved
+ * @link     http://www.carltonsoftware.co.uk
+ */
+
+namespace HelloWorld;
+
+
+/**
+ * A test class
+ *
+ * @category  HelloWorld
+ * @package   HelloWorld
+ * @author    Pete Lawrence <pete@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   All rights reserved
+ * @link      http://www.carltonsoftware.co.uk
+ */
+class HelloWorld
+{
+
+    /**
+     * Prints hello world
+     *
+     * @return void
+     */
+    public function getMessage()
+    {
+        return 'Hello World!';
+    }
+
+}
--- src/test.php
+++ src/test.php
@@ -1,38 +1,6 @@
 <?php
-/**
- * A test class
- *
- * PHP version 5
- *
- * @category Test
- * @package  Test
- * @author   Pete Lawrence <pete@carltonsoftware.co.uk>
- * @license  All rights reserved
- * @link     http://www.carltonsoftware.co.uk
- */
 
+require_once '../vendor/autoload.php';
 
-/**
- * A test class
- *
- * @category  Test
- * @package   Test
- * @author    Pete Lawrence <pete@carltonsoftware.co.uk>
- * @copyright 2014 Carlton Software
- * @license   All rights reserved
- * @link      http://www.carltonsoftware.co.uk
- */
-class HelloWorld
-{
-
-    /**
-     * Prints hello world
-     *
-     * @return void
-     */
-    public function getMessage()
-    {
-        return 'Hello World!';
-    }
-
-}
+$helloWorld = new HelloWorld\HelloWorld();
+print $helloWorld->getMessage();
